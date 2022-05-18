f7ed469650426a7ac9e33ced6db3d58ba964ddd5
Forevgit <SaneXTRI@Gmail.com>
2022-05-18 19:15:37 +0300

    bot

--- bot.py
+++ bot.py
@@ -18,32 +18,45 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['Start'])
 def welcome(message):
-		bot.send_message(message.chat.id,'Hello, im bot for your timetable, use "/help" to see my options')
+		bot.send_message(message.chat.id,'Hello, im bot for your timetable, use "/Help" to see my options')
 
 @bot.message_handler(commands = ['Help'])
 def help(message):
 	bot.send_message(message.chat.id, """Available Commands :-
 	/link - To get the Link to a class
 	/Info - To get info about your professor
-	/Shedule - To get shedule your class
-	/Add - Add your info""")
+	/Schedule - To get shedule your class
+	/Add - Add your info
+	/Delete - Delete all info""")
 
 
 @bot.message_handler(commands = ['link'])
 def link(message):
-	bot.send_message(message.chat.id,
-		"!!!!")
+	links = get_link()
+	answer = ""
+	for lesson,link in links:
+ 		answer += f"{lesson}: {link}\n\n\n"
+	bot.send_message(message.chat.id,answer)
+
 
 @bot.message_handler(commands = ['Info'])
 def info(message):
-	bot.send_message(message.chat.id,
-		"!!!")
+	for i in range(len(get_info())):
+		bot.send_message(message.chat.id, " викладає: ".join(get_info()[i]))
+	
 
 
-@bot.message_handler(commands = ['Shedule'])
+@bot.message_handler(commands = ['Schedule'])
 def shedule(message):
-	for i in range(len(Shedules)):
-		bot.send_message(message.chat.id, ",".join(Shedules[i]))
+	#schedule = get_schedule()
+	#answer = schedule(0,2)
+	for i in range(len(get_schedule())):
+		bot.send_message(message.chat.id, ", ".join(get_schedule()[i]))
+
+@bot.message_handler(commands = ['Delete'])
+def shedule(message):
+	delete_all()
+	bot.send_message(message.chat.id,"Your schedule was delete")
 
 @bot.message_handler(commands = ['Add'])
 def get_message(message):
@@ -54,8 +67,8 @@ def get_message(message):
 	day = message.text
 
 	bot.send_message(message.chat.id,	
-		"""Enter lessons, time, link:
-		(Example: Math,15:00,/Link..,Chemistry,16:45,/Link..,...)""")
+		"""Enter lessons, time, link, Professor:
+		(Example: Чисельні методи,15:00,/Link..,Світлана Борисівна,ПРП,16:45,/Link..,Леся Ігорівна,...)""")
 
 	bot.register_next_step_handler(message,get_lessons_and_time, day=day)
 
@@ -65,9 +78,10 @@ def get_lessons_and_time(message,day):
 	user_input = user_input.split(',')
 
 	
-	bot.send_message(message.chat.id, f"Your shedule on {day}")
-	for i in range(0,len(user_input),3):
-		bot.send_message(message.chat.id, f'{user_input[i]} {user_input[i+1]} {user_input[i+2]}')
-	input_bd(user_input)
+	#bot.send_message(message.chat.id, f"Your shedule on {day}")
+	#for i in range(0,len(user_input),3):
+	#	bot.send_message(message.chat.id, f'{user_input[i]} {user_input[i+1]} {user_input[i+2]}')
+	input_bd(day,user_input)
+
 
 bot.polling()
--- dateb.py
+++ dateb.py
@@ -2,9 +2,9 @@ import pymysql
 from config import host, user, password, db_name
 import datetime
 
-Links = list()
-Infors = list()
-Shedules = list()
+
+
+
 try:
 	connection = pymysql.connect(
 	host = "localhost",
@@ -15,44 +15,54 @@ try:
 	)
 
 	print("Successfully connected")
+	def create_db():
+		with connection.cursor() as cursor:
+			create_table_query = "CREATE TABLE `users`(id int AUTO_INCREMENT,Day varchar(32),Lesson varchar(64), Name_lastname varchar(64),Link varchar(256),Time Time, PRIMARY KEY (id))"
+			cursor.execute(create_table_query)
+			print("Table created successfully")
 
-#	with connection.cursor() as cursor:
-#		insert_query = "INSERT INTO `tt`(`id`, `Name`, `Time`, `Link`, `Day`) VALUES ('2','Max','11:21','dldldd','Tuesday')"
-#		cursor.execute(insert_query)
-#		connection.commit()
-	#with connection.cursor() as cursor:
-	#	create_table_query = "CREATE TABLE `users`(id int AUTO_INCREMENT,Link varchar(32),Time Time,Day varchar(32), PRIMARY KEY (id))"
-	#	cursor.execute(create_table_query)
-	#	print("Table created successfully")
-	with connection.cursor() as cursor:
-		cursor.execute("SELECT Day FROM `tt`;")
-		rows = cursor.fetchall()
-		for row in rows:
-			Links.append(row)
-	with connection.cursor() as cursor:
-		cursor.execute("SELECT Link FROM `tt`;")
-		rows = cursor.fetchall()
-		for row in rows:
-			Infors.append(row)
-	with connection.cursor() as cursor:
-		cursor.execute("SELECT Day, Time, Link FROM `users`;")
-		rows = cursor.fetchall()
+	def get_info():
+		Infors = list()
+		with connection.cursor() as cursor:
+			cursor.execute("SELECT distinct Name_lastname, Lesson FROM `users`;")
+			rows = cursor.fetchall()
+			for row in rows:
+				Infors.append(row)
+		return Infors
 
-		for row in rows:
-			row = list(row)
-			row[1]=str((datetime.datetime.min + row[1]).time())[:-3]
-			Shedules.append(row)
-		print(Shedules)
-	
+	def get_link():	
+		Links = list()	
+		with connection.cursor() as cursor:
+			cursor.execute("SELECT distinct Lesson, Link FROM `users`;")
+			rows = cursor.fetchall()
+			for row in rows:
+				Links.append(row)
+		return Links
+	def get_schedule():	
+		Shedules = list()	
+		with connection.cursor() as cursor:
+			cursor.execute("SELECT Lesson, Time, Day FROM `users`;")
+			rows = cursor.fetchall()
 
+			for row in rows:
+				row = list(row)
+				row[1]=str((datetime.datetime.min + row[1]).time())[:-3]
+				Shedules.append(row)
+		return Shedules
 except Exception as ex:
 	print("Connection refused")
 	print(ex)
 
-def input_bd(user_input):
+def input_bd(day,user_input):
 	with connection.cursor() as cursor:
-		for i in range(0, len(user_input),3):
-			insert_query = f"INSERT INTO `users`(`Day`, `Time`, `Link`) VALUES ('{user_input[i]}', '{user_input[i+1]}', '{user_input[i+2]}')"
+		for i in range(0, len(user_input),4):
+			insert_query = f"INSERT INTO `users`(`Day`,`Lesson`, `Time`, `Link`, `Name_lastname`) VALUES ('{day}', '{user_input[i]}', '{user_input[i+1]}', '{user_input[i+2]}', '{user_input[i+3]}')"
 			cursor.execute(insert_query)
-			print(insert_query)
+	connection.commit()
+
+
+def delete_all():
+	with connection.cursor() as cursor:
+		delete_all = "DELETE FROM `users`;"
+		cursor.execute(delete_all)
 	connection.commit()
