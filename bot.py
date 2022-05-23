c46d4ad286e67b64b93c0fe44b4e927593933321
Forevgit <SaneXTRI@Gmail.com>
2022-05-23 15:38:46 +0300

    Bot

--- bot.py
+++ bot.py
@@ -16,18 +16,18 @@ bot = telebot.TeleBot(API_TOKEN)
 
 
 
-@bot.message_handler(commands=['Start'])
+@bot.message_handler(commands=['start'])
 def welcome(message):
-		bot.send_message(message.chat.id,'Hello, im bot for your timetable, use "/Help" to see my options')
+		bot.send_message(message.chat.id,'Привіт, я бот для вашого розкладу предметів/уроків, введіть команду "/help" щоб побачити доступні команди')
 
-@bot.message_handler(commands = ['Help'])
+@bot.message_handler(commands = ['help'])
 def help(message):
-	bot.send_message(message.chat.id, """Available Commands :-
-	/link - To get the Link to a class
-	/Info - To get info about your professor
-	/Schedule - To get shedule your class
-	/Add - Add your info
-	/Delete - Delete all info""")
+	bot.send_message(message.chat.id, """Доступні команди :-
+	/link - Отримати посилання на пару
+	/Info - Отримати інформацію про викладача і пару яку він проводить
+	/Schedule - Отримати ваш розклад 
+	/Add - Добавити вашу інформацію
+	/Delete - Видалити вашу інформацію""")
 
 
 @bot.message_handler(commands = ['link'])
@@ -41,46 +41,49 @@ def link(message):
 
 @bot.message_handler(commands = ['Info'])
 def info(message):
-	for i in range(len(get_info())):
-		bot.send_message(message.chat.id, " викладає: ".join(get_info()[i]))
+	answer = ""
+	infors = get_info()
+	for Name_lastname,Lesson in infors:
+		answer += f'{Name_lastname}: {Lesson}\n\n'
+	bot.send_message(message.chat.id, answer)
 	
 
 
 @bot.message_handler(commands = ['Schedule'])
 def shedule(message):
-	#schedule = get_schedule()
-	#answer = schedule(0,2)
-	for i in range(len(get_schedule())):
-		bot.send_message(message.chat.id, ", ".join(get_schedule()[i]))
+	days = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'Пятниця']
+	answer = ""
+	for day in days:
+		answer += day + ':\n'
+		schedule = get_schedule(day)
+		for lesson,time in schedule:
+			answer += f'{lesson}: {time}\n'
+		answer += "\n"
+	bot.send_message(message.chat.id, answer)
 
 @bot.message_handler(commands = ['Delete'])
 def shedule(message):
 	delete_all()
-	bot.send_message(message.chat.id,"Your schedule was delete")
+	bot.send_message(message.chat.id,"Ваша інформація видалена")
 
 @bot.message_handler(commands = ['Add'])
 def get_message(message):
 	
-	bot.send_message(message.chat.id,"Enter day:")
+	bot.send_message(message.chat.id,"Введіть день:")
 	bot.register_next_step_handler(message,get_message)
 def get_message(message):
 	day = message.text
 
 	bot.send_message(message.chat.id,	
-		"""Enter lessons, time, link, Professor:
-		(Example: Чисельні методи,15:00,/Link..,Світлана Борисівна,ПРП,16:45,/Link..,Леся Ігорівна,...)""")
+		"""Введіть ваш предмет, час, посилання, викладач:
+		(Приклад: Чисельні методи, 15:00, /Link.., Світлана Борисівна, ПРП, 16:45, /Link.., Леся Ігорівна,...)""")
 
 	bot.register_next_step_handler(message,get_lessons_and_time, day=day)
 
 def get_lessons_and_time(message,day):
 	
 	user_input = message.text
-	user_input = user_input.split(',')
-
-	
-	#bot.send_message(message.chat.id, f"Your shedule on {day}")
-	#for i in range(0,len(user_input),3):
-	#	bot.send_message(message.chat.id, f'{user_input[i]} {user_input[i+1]} {user_input[i+2]}')
+	user_input = user_input.split(', ')
 	input_bd(day,user_input)
 
 
--- dateb.py
+++ dateb.py
@@ -11,44 +11,11 @@ try:
   	port = 3306,
  	user = "root",
   	password = "Nikewasd2015",
-  	database = "test",
+  	database = "timetable",
 	)
 
 	print("Successfully connected")
-	def create_db():
-		with connection.cursor() as cursor:
-			create_table_query = "CREATE TABLE `users`(id int AUTO_INCREMENT,Day varchar(32),Lesson varchar(64), Name_lastname varchar(64),Link varchar(256),Time Time, PRIMARY KEY (id))"
-			cursor.execute(create_table_query)
-			print("Table created successfully")
 
-	def get_info():
-		Infors = list()
-		with connection.cursor() as cursor:
-			cursor.execute("SELECT distinct Name_lastname, Lesson FROM `users`;")
-			rows = cursor.fetchall()
-			for row in rows:
-				Infors.append(row)
-		return Infors
-
-	def get_link():	
-		Links = list()	
-		with connection.cursor() as cursor:
-			cursor.execute("SELECT distinct Lesson, Link FROM `users`;")
-			rows = cursor.fetchall()
-			for row in rows:
-				Links.append(row)
-		return Links
-	def get_schedule():	
-		Shedules = list()	
-		with connection.cursor() as cursor:
-			cursor.execute("SELECT Lesson, Time, Day FROM `users`;")
-			rows = cursor.fetchall()
-
-			for row in rows:
-				row = list(row)
-				row[1]=str((datetime.datetime.min + row[1]).time())[:-3]
-				Shedules.append(row)
-		return Shedules
 except Exception as ex:
 	print("Connection refused")
 	print(ex)
@@ -66,3 +33,32 @@ def delete_all():
 		delete_all = "DELETE FROM `users`;"
 		cursor.execute(delete_all)
 	connection.commit()
+
+def get_info():
+	Infors = list()
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT distinct Name_lastname, Lesson FROM `users`;")
+		rows = cursor.fetchall()
+		for row in rows:
+			Infors.append(row)
+	return Infors
+
+def get_link():	
+	Links = list()	
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT distinct Lesson, Link FROM `users`;")
+		rows = cursor.fetchall()
+		for row in rows:
+			Links.append(row)
+	return Links
+def get_schedule(day):	
+	Shedules = list()	
+	with connection.cursor() as cursor:
+		cursor.execute(f"SELECT Lesson, Time FROM `users` WHERE Day='{day}' ORDER BY Time;")
+		rows = cursor.fetchall()
+
+		for row in rows:
+			row = list(row)
+			row[1]=str((datetime.datetime.min + row[1]).time())[:-3]
+			Shedules.append(row)
+	return Shedules
